c17d3e1180f47e58e1d5517e88e709be9f244863
broot <bananarobot@naver.com>
2019-12-23 19:24:45 +0900

    update

--- init.el
+++ init.el
@@ -21,7 +21,6 @@
     (mapc load-it (directory-files dir nil "\\.el"))))
 
 (load-directory "~/.emacs.d/config")
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,7 +33,7 @@
      ("gnu" . "https://elpa.gnu.org/packages/"))))
  '(package-selected-packages
    (quote
-    (company-lsp lsp-ui lsp-mode golden-ratio solaire-mode rustic mood-line minions leaf-keywords flycheck evil doom-themes counsel company))))
+    (rustic company-lsp lsp-ui lsp-mode golden-ratio solaire-mode minions mood-line doom-themes flycheck company counsel ivy evil leaf-keywords leaf))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
